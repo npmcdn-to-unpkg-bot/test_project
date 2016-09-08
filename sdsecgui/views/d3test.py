0437c8178b0c23af35a198e3d68e995357cbf2f1
jinsikKang <kangjinsik@gmail.com>
2016-09-08 15:20:38 +0900

    dd

--- sdsecgui/views/d3test.py
+++ sdsecgui/views/d3test.py
@@ -1,12 +1,7 @@
 #_*_coding:utf-8_*_
 
-from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 
-from sdsecgui.models import Post
-from sdsec import log_handler
 from sdsec.log_handler import setLogDir, getLogger
 
 setLogDir()
