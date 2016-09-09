ea2655f4ad1fdbeb580990fe5465153e7b3e9327
jinsikKang <kangjinsik@gmail.com>
2016-09-09 10:21:21 +0900

    dd

--- sdsecgui/cmodels/instance.py
+++ sdsecgui/cmodels/instance.py
@@ -7,11 +7,24 @@ setLogDir()
 logger = getLogger()
 
 class Instance:
-    def showInstanceById(self, id):
+    def outputToInfo(self, infoDic, outputList):
+        for row in outputList[3:-1]:
+            cols = row[1:-1].split("|")
+            key = cols[0].strip().replace(" ", "_")
+            value = cols[1].strip()
+            if infoDic.get(key):
+                infoDic[key] = value
+            else:
+                raise Exception, key + "에 해당하는 키가 없습니다."
+        return infoDic
+
+    def showInfoById(self, id):
+        # id로 인스턴스를 찾는다.
         logger.debug("showInstanceById")
         output = excuteCmd("nova show " + id)
-        rows = output.splitlines()
-        if rows:
+
+        outputList = output.splitlines()
+        if outputList:
             instanceDic = {
                 "OS-DCF:diskConfig": "",
                 "OS-EXT-AZ:availability_zone": "",
@@ -54,61 +67,60 @@ class Instance:
                 "updated": "",
                 "user_id": "",
             }
-            for row in rows[3:-1]:
-                cols = row[1:-1].split("|")
-                key = cols[0].strip().replace(" ", "_")
-                value = cols[1].strip()
-                instanceDic[key] = value
-            return instanceDic
+            return self.outputToInfo(instanceDic, outputList)
         else:
             logger.debug(id + "에 해당하는 인스턴스가 없습니다.")
             return None
+
     def __init__(self, id):
-        # tempList = self.parsingOutputToList(output)
-        instanceDic = self.showInstanceById(id)
+        instanceDic = self.showInfoById(id)
+        if instanceDic == None:
+            raise Exception, id + "에 해당하는 인스턴스가 없습니다."
         self.os_dce = {}
         self.os_ext_az = {}
         self.os_ext_srv_attr = {}
         self.os_ext_sts = {}
         self.os_srv_usg = {}
         self.os_extended_volumes = {}
-        self.os_dce["diskConfig"] =                    instanceDic["OS-DCF:diskConfig"]
-        self.os_ext_az["availability_zone"] =          instanceDic["OS-EXT-AZ:availability_zone"]
-        self.os_ext_srv_attr["host"] =                 instanceDic["OS-EXT-SRV-ATTR:host"]
-        self.os_ext_srv_attr["hostname"] =             instanceDic["OS-EXT-SRV-ATTR:hostname"]
-        self.os_ext_srv_attr["hypervisor_hostnam"] =   instanceDic["OS-EXT-SRV-ATTR:hypervisor_hostnam"]
-        self.os_ext_srv_attr["instance_name"] =        instanceDic["OS-EXT-SRV-ATTR:instance_name"]
-        self.os_ext_srv_attr["kernel_id"] =            instanceDic["OS-EXT-SRV-ATTR:kernel_id"]
-        self.os_ext_srv_attr["launch_index"] =         instanceDic["OS-EXT-SRV-ATTR:launch_index"]
-        self.os_ext_srv_attr["ramdisk_id"] =           instanceDic["OS-EXT-SRV-ATTR:ramdisk_id"]
-        self.os_ext_srv_attr["reservation_id"] =       instanceDic["OS-EXT-SRV-ATTR:reservation_id"]
-        self.os_ext_srv_attr["root_device_name"] =     instanceDic["OS-EXT-SRV-ATTR:root_device_name"]
-        self.os_ext_srv_attr["user_data"] =            instanceDic["OS-EXT-SRV-ATTR:user_data"]
-        self.os_ext_sts["power_state"] =               instanceDic["OS-EXT-STS:power_state"]
-        self.os_ext_sts["task_state"] =                instanceDic["OS-EXT-STS:task_state"]
-        self.os_ext_sts["vm_state"] =                  instanceDic["OS-EXT-STS:vm_state"]
-        self.os_srv_usg["launched_at"] =               instanceDic["OS-SRV-USG:launched_at"]
-        self.os_srv_usg["terminated_at"] =             instanceDic["OS-SRV-USG:terminated_at"]
-        self.accessIPv4 =                              instanceDic["accessIPv4"]
-        self.accessIPv6 =                              instanceDic["accessIPv6"]
-        self.config_drive =                            instanceDic["config_drive"]
-        self.created =                                 instanceDic["created"]
-        self.description =                             instanceDic["description"]
-        self.flavor =                                  instanceDic["flavor"]
-        self.hostId =                                  instanceDic["hostId"]
-        self.host_status =                             instanceDic["host_status"]
-        self.id =                                      instanceDic["id"]
-        self.image =                                   instanceDic["image"]
-        self.key_name =                                instanceDic["key_name"]
-        self.locked =                                  instanceDic["locked"]
-        self.metadata =                                instanceDic["metadata"]
-        self.name =                                    instanceDic["name"]
+
+        self.os_dce["diskConfig"]                    = instanceDic["OS-DCF:diskConfig"]
+        self.os_ext_az["availability_zone"]          = instanceDic["OS-EXT-AZ:availability_zone"]
+        self.os_ext_srv_attr["host"]                 = instanceDic["OS-EXT-SRV-ATTR:host"]
+        self.os_ext_srv_attr["hostname"]             = instanceDic["OS-EXT-SRV-ATTR:hostname"]
+        self.os_ext_srv_attr["hypervisor_hostnam"]   = instanceDic["OS-EXT-SRV-ATTR:hypervisor_hostnam"]
+        self.os_ext_srv_attr["instance_name"]        = instanceDic["OS-EXT-SRV-ATTR:instance_name"]
+        self.os_ext_srv_attr["kernel_id"]            = instanceDic["OS-EXT-SRV-ATTR:kernel_id"]
+        self.os_ext_srv_attr["launch_index"]         = instanceDic["OS-EXT-SRV-ATTR:launch_index"]
+        self.os_ext_srv_attr["ramdisk_id"]           = instanceDic["OS-EXT-SRV-ATTR:ramdisk_id"]
+        self.os_ext_srv_attr["reservation_id"]       = instanceDic["OS-EXT-SRV-ATTR:reservation_id"]
+        self.os_ext_srv_attr["root_device_name"]     = instanceDic["OS-EXT-SRV-ATTR:root_device_name"]
+        self.os_ext_srv_attr["user_data"]            = instanceDic["OS-EXT-SRV-ATTR:user_data"]
+        self.os_ext_sts["power_state"]               = instanceDic["OS-EXT-STS:power_state"]
+        self.os_ext_sts["task_state"]                = instanceDic["OS-EXT-STS:task_state"]
+        self.os_ext_sts["vm_state"]                  = instanceDic["OS-EXT-STS:vm_state"]
         self.os_extended_volumes["volumes_attached"] = instanceDic["os-extended-volumes:volumes_attached"]
-        self.progress =                                instanceDic["progress"]
-        self.public_network =                          instanceDic["public_network"]
-        self.security_groups =                         instanceDic["security_groups"]
-        self.status =                                  instanceDic["status"]
-        self.tags =                                    instanceDic["tags"]
-        self.tenant_id =                               instanceDic["tenant_id"]
-        self.updated =                                 instanceDic["updated"]
-        self.user_id =                                 instanceDic["user_id"]
+        self.os_srv_usg["launched_at"]               = instanceDic["OS-SRV-USG:launched_at"]
+        self.os_srv_usg["terminated_at"]             = instanceDic["OS-SRV-USG:terminated_at"]
+
+        self.accessIPv4                              = instanceDic["accessIPv4"]
+        self.accessIPv6                              = instanceDic["accessIPv6"]
+        self.config_drive                            = instanceDic["config_drive"]
+        self.created                                 = instanceDic["created"]
+        self.description                             = instanceDic["description"]
+        self.flavor                                  = instanceDic["flavor"]
+        self.hostId                                  = instanceDic["hostId"]
+        self.host_status                             = instanceDic["host_status"]
+        self.id                                      = instanceDic["id"]
+        self.image                                   = instanceDic["image"]
+        self.key_name                                = instanceDic["key_name"]
+        self.locked                                  = instanceDic["locked"]
+        self.metadata                                = instanceDic["metadata"]
+        self.name                                    = instanceDic["name"]
+        self.progress                                = instanceDic["progress"]
+        self.public_network                          = instanceDic["public_network"]
+        self.security_groups                         = instanceDic["security_groups"]
+        self.status                                  = instanceDic["status"]
+        self.tags                                    = instanceDic["tags"]
+        self.tenant_id                               = instanceDic["tenant_id"]
+        self.updated                                 = instanceDic["updated"]
+        self.user_id                                 = instanceDic["user_id"]
--- sdsecgui/tools/command.py
+++ sdsecgui/tools/command.py
@@ -11,6 +11,7 @@ logger = getLogger()
 
 
 def excuteCmd(command):
+    # 명령 실행, 출력 반환
     logger.debug("excuteCmd")
     f = os.popen(command)
     result = f.read()
@@ -19,22 +20,31 @@ def excuteCmd(command):
 
 
 def parsingOutputToList(output):
+    # 출력을 받아 parsing함
     rows = output.splitlines()
     if rows:
+        # index 0 = 테두리
+        # index 1 = 키(컬럼명)
         keyList = rows[1][1:-1].split("|")
         for idx, key in enumerate(keyList):
+            # 양끝 공백을 없애고 사이공백은 밑줄로 전환
             keyList[idx] = key.lower().strip().replace(" ", "_")
         resultList = []
         for row in rows[3:-1]:
+            # index 3 ~ = 목록
+            # 한 줄당 하나의 인스턴스
+            # 하나의 인스턴스를 Dictionary에 담는다
             resultDic = {}
             cols = row[1:-1].split("|")
             for idx, col in enumerate(cols):
                 key = keyList[idx]
                 value = col.strip()
                 resultDic[key] = value
+            # Dictionary를 List에 담아 반환한다.
             resultList.append(resultDic)
         return resultList
     else:
+        # 인스턴스가 하나도 없을 때
         logger.debug("List is empty")
         return None
 
