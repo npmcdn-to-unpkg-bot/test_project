f7a722ebbc04528ccc689e63e36615db4577307a
jinsikKang <kangjinsik@gmail.com>
2016-09-09 10:32:02 +0900

    dd

--- sdsecgui/cmodels/instance.py
+++ sdsecgui/cmodels/instance.py
@@ -70,7 +70,7 @@ class Instance:
             }
             return self.outputToInfo(instanceDic, outputList)
         else:
-            logger.debug(str(u"'"+unicode(id) + u"' 에 해당하는 인스턴스가 없습니다."))
+            logger.debug(str("'" + id + "' 에 해당하는 인스턴스가 없습니다."))
             return None
 
     def __init__(self, id):
